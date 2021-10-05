3268111fa697693593b8f09864dc141aaa04a975
Stella Martin <stellamartin222@gmail.com>
2021-10-05 15:30:21 +0100

    Entering Players completed

--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,16 @@
 feature 'Testing infrastucture' do
   scenario 'Can run app and display page' do
     visit('/')
-    expect(page).to have_content 'Testing infrastucture'
+    expect(page).to have_content 'submit'
+  end
+
+  scenario 'will store names given by the player' do
+    visit('/')
+    within("#name_form") do
+      fill_in 'player_1_name', with: 'stella'
+      fill_in 'player_2_name', with: 'troy'
+    end
+    click_button 'submit'
+    expect(page).to have_content 'Player 1 name is stella Player 2 name is troy'
   end
 end
